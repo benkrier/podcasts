7671804b2bba7dc04e0bc43e6db16e935a54183b
Benjamin Krier <benkrier@gmail.com>
2017-03-24 12:36:15 -0500

    Add publication and subscription

--- client/templates/podcasts.js
+++ client/templates/podcasts.js
@@ -2,6 +2,10 @@ import { Template } from 'meteor/templating';
 import { Podcasts } from '../../collections/podcasts.js';
 import './podcasts.html';
 
+Template.podcasts.onCreated(() => {
+  Template.instance().subscribe('podcasts');
+});
+
 Template.podcasts.helpers({
   podcasts() {
     return Podcasts.find();
